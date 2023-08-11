ac4015e51fa591c519cbdedceae2ee0727076a22
shoyabsiddique0 <shoyab.s@mypcot.com>
2023-08-11 14:49:42 +0530

    ebook

--- lib/EpubxReader/epub_web_view.dart
+++ lib/EpubxReader/epub_web_view.dart
@@ -13,9 +13,35 @@ class EpubWebView extends StatefulWidget {
 
 class _EpubWebViewState extends State<EpubWebView> {
   double dyanmicFont = 15;
+  PageController controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: FutureBuilder(
+            future: widget.chapter,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                          title: Text(snapshot.data![index].Title.toString()),
+                          onTap: () => controller.animateToPage(index,
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.bounceIn),
+                          subtitle: Column(
+                            children: snapshot.data![index].SubChapters!
+                                .map((e) => ListTile(
+                                      title: Text(e.Title.toString()),
+                                    ))
+                                .toList(),
+                          ));
+                    });
+              }
+              return Text("");
+            }),
+      ),
       appBar: AppBar(
         title: Text(widget.title),
         actions: [
@@ -36,33 +62,64 @@ class _EpubWebViewState extends State<EpubWebView> {
               icon: Icon(Icons.minimize_outlined)),
         ],
       ),
-      body: SingleChildScrollView(
-        child: StreamBuilder(
-            stream: widget.chapter.asStream(),
-            builder: (context, snapshot) {
+      body: StreamBuilder(
+          stream: widget.chapter.asStream(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List data = snapshot.data!;
               return snapshot.hasData
-                  ? Column(
-                      children: snapshot.data!.map((e) {
-                        // String data = await e.readHtmlContent();
+                  ? PageView(
+                      scrollDirection: Axis.horizontal,
+                      physics: NeverScrollableScrollPhysics(),
+                      controller: controller,
+                      // itemCount: data.length,
+                      children: data.map((e) {
                         return FutureBuilder<String>(
                             future: e.readHtmlContent(),
                             builder: (context, snapshot1) {
-                              return Html(
-                                data: snapshot1.hasData ? snapshot1.data : "",
-                                style: {
-                                  "body": Style(
-                                    fontSize: FontSize(dyanmicFont),
+                              return SingleChildScrollView(
+                                child: GestureDetector(
+                                  onPanUpdate: (details) {
+                                    if (details.delta.dx > 0) {
+                                      print("Go Back");
+                                      print(controller.page!.toInt() - 1);
+                                      controller.animateToPage(
+                                          controller.page!.toInt() - 1,
+                                          duration: Duration(milliseconds: 10),
+                                          curve: Curves.bounceIn);
+                                    } else if (details.delta.dx < 0) {
+                                      print("Go Aage");
+                                      print(controller.page!.toInt() + 1);
+                                      controller.animateToPage(
+                                          controller.page!.toInt() + 1,
+                                          duration: Duration(milliseconds: 10),
+                                          curve: Curves.bounceIn);
+                                    }
+                                  },
+                                  child: SizedBox(
+                                    // height: 400,
+                                    width: MediaQuery.of(context).size.width,
+                                    child: Html(
+                                      data: snapshot1.hasData
+                                          ? snapshot1.data
+                                          : "",
+                                      style: {
+                                        "body": Style(
+                                          fontSize: FontSize(dyanmicFont),
+                                        ),
+                                        "h1": Style(color: Colors.blue),
+                                        "h2": Style(color: Colors.amber)
+                                      },
+                                    ),
                                   ),
-                                  "h1": Style(color: Colors.blue),
-                                  "h2": Style(color: Colors.amber)
-                                },
+                                ),
                               );
                             });
-                      }).toList(),
-                    )
+                      }).toList())
                   : Text("Data");
-            }),
-      ),
+            }
+            return Text("");
+          }),
     );
   }
 }
